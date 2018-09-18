4e67ba9f0efc31e388b7ccff6fb5bf4ce019eaff
t00198396 <adam.hurley@students.ittralee.ie>
2018-09-18 17:35:09 +0100

    Exercise1

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -4,8 +4,27 @@ import java.util.Scanner;
 public class Exercise1 {
     public static void main(String[] args) {
 
-        float exchangeRate;
+        float exchangeRate, poundValue;
 
         Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter an exchange rate: ");
+        exchangeRate = input.nextFloat();
+
+        System.out.print("Please enter a £ value: ");
+        poundValue= input.nextFloat();
+
+
+
+        while(poundValue>0)
+        {
+            System.out.print("Euro Value: "+ String.format("%.2f",poundValue*exchangeRate) + "\nPound Value: " + poundValue );
+
+            System.out.print("\nPlease enter a £ value: ");
+            poundValue= input.nextFloat();
+
+
+
+        }
     }
 }

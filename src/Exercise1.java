b41489aebb9947627b3f0b6c873c2f37399625d9
t00198396 <adam.hurley@students.ittralee.ie>
2018-09-18 17:02:03 +0100

    Exercise1

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
+
 public class Exercise1 {
+    public static void main(String[] args) {
+
+        float exchangeRate;
+
+        Scanner input = new Scanner(System.in);
+    }
 }

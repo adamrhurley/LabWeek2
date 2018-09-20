88d242067aca77cca91d12bd1a40376a713c46aa
t00198396 <adam.hurley@students.ittralee.ie>
2018-09-20 12:48:01 +0100

    Exercise3

--- src/Exercise3.java
+++ src/Exercise3.java
@@ -5,7 +5,7 @@ public class Exercise3 {
     public static void main(String[] args)
     {
         float length, breadth, area, costPer, cost;
-        String message, name, lengthAsString, breadthAsString, areaAsString, costPerAsString, costAsString;
+        String message, name, lengthAsString, breadthAsString, costPerAsString;
 
         name = JOptionPane.showInputDialog("Please enter your name: ");
 
@@ -15,11 +15,16 @@ public class Exercise3 {
         breadthAsString = JOptionPane.showInputDialog("Please enter the breadth of the room: ");
         breadth = Float.parseFloat(breadthAsString);
 
+        costPerAsString = JOptionPane.showInputDialog("Please enter the cost per square meter of carpet: ");
+        costPer = Float.parseFloat(costPerAsString);
 
+        area = length*breadth;
+        cost = area*costPer;
 
-
-
-        message = "Quotation  for "+ name + "\n" + "Length of room: " + length + "\nBreadth of room: " + breadth;
+        message = "Quotation for "+ name + "\n" + "Length of room: " + length + " m" + "\nBrea" +
+                "dth of room: " + breadth + " m" + "\nTotal area of the room: " + area + " sq." +
+                " m" + "\nCost per square meter of carpet: " + costPer + " euro" + "\nTotal cost of " +
+                "carpet: " + cost + " euro";
 
         JOptionPane.showMessageDialog(null,message,"Quotation ",JOptionPane.PLAIN_MESSAGE);
 

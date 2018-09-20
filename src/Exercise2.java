209c11da972815d77fc2ef500a348b308dccd54f
t00198396 <adam.hurley@students.ittralee.ie>
2018-09-20 10:50:39 +0100

    Exercise2

--- src/Exercise2.java
+++ src/Exercise2.java
@@ -1,7 +1,22 @@
-import java.util.Scanner;
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
+import java.awt.Font;
+
 
 public class Exercise2 {
+    public static void main(String[] args)
+    {
+        int i=1;
+        float inches;
+
+        JTextArea textArea = new JTextArea(14,30);
+
+        Font textAreaFont = new Font("monospaced",Font.PLAIN,12);
 
 
-    Scanner input = new Scanner(System.in);
+    }
+
+
+
+
 }

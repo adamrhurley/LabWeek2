e5ec35e90dba6ac4440a5650b3ab1b86ff009b92
t00198396 <adam.hurley@students.ittralee.ie>
2018-09-20 15:17:28 +0100

    Exercise5

---
+++ src/Exercise5.java
@@ -0,0 +1,13 @@
+import static java.lang.Math.sqrt;
+
+public class Exercise5 {
+    public static void main(String[] args)
+    {
+        double x, a = 1, b = 6, c =-16;
+
+        x=(-(b)-sqrt((b*b)-4*a*c))/2*a;
+
+        System.out.print(x);
+
+    }
+}
